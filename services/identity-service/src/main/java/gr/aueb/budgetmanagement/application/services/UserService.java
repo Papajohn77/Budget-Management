34aef7cbfaa6399e9d8b4555065d56c542d4fc81
Papajohn77 <t8190314@aueb.gr>
2026-05-23 13:45:04 +0300

    Update Identity's User entity to remove unrelated code

--- services/identity-service/src/main/java/gr/aueb/budgetmanagement/application/services/UserService.java
+++ services/identity-service/src/main/java/gr/aueb/budgetmanagement/application/services/UserService.java
@@ -4,10 +4,8 @@ import gr.aueb.budgetmanagement.application.commands.AuthenticateUserCommand;
 import gr.aueb.budgetmanagement.application.commands.RegisterUserCommand;
 import gr.aueb.budgetmanagement.application.exceptions.AlreadyExistsException;
 import gr.aueb.budgetmanagement.application.exceptions.InvalidCredentialsException;
-import gr.aueb.budgetmanagement.application.exceptions.NotFoundException;
 import gr.aueb.budgetmanagement.application.repositories.UserRepository;
 import gr.aueb.budgetmanagement.application.representations.AccessTokenRepresentation;
-import gr.aueb.budgetmanagement.application.representations.BalanceRepresentation;
 import gr.aueb.budgetmanagement.domain.entities.User;
 import gr.aueb.budgetmanagement.domain.ports.PasswordHasher;
 import gr.aueb.budgetmanagement.infrastructure.security.JwtTokenService;
@@ -67,13 +65,5 @@ public class UserService {
         String accessToken = jwtTokenService.generateToken(user);
 
         return new AccessTokenRepresentation("Bearer", accessToken);
-    }
-
-    @Transactional
-    public BalanceRepresentation getBalance(Long userId) {
-        User user = userRepository.findById(userId)
-            .orElseThrow(() -> new NotFoundException("User not found with id: " + userId));
-
-        return new BalanceRepresentation(user.getCurrentBalance());
     }
 }
--- services/identity-service/src/main/java/gr/aueb/budgetmanagement/presentation/api/resources/UserResource.java
+++ services/identity-service/src/main/java/gr/aueb/budgetmanagement/presentation/api/resources/UserResource.java
@@ -1,21 +1,14 @@
 package gr.aueb.budgetmanagement.presentation.api.resources;
 
-import org.eclipse.microprofile.jwt.JsonWebToken;
-
 import gr.aueb.budgetmanagement.application.commands.AuthenticateUserCommand;
 import gr.aueb.budgetmanagement.application.commands.RegisterUserCommand;
-import gr.aueb.budgetmanagement.application.exceptions.InvalidCredentialsException;
 import gr.aueb.budgetmanagement.application.representations.AccessTokenRepresentation;
-import gr.aueb.budgetmanagement.application.representations.BalanceRepresentation;
 import gr.aueb.budgetmanagement.application.services.UserService;
 import gr.aueb.budgetmanagement.presentation.api.requests.AuthenticateUserRequest;
 import gr.aueb.budgetmanagement.presentation.api.requests.RegisterUserRequest;
-import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.core.SecurityContext;
 
 @Path("/api/v1/users")
 public class UserResource {
@@ -51,23 +44,6 @@ public class UserResource {
         );
 
         AccessTokenRepresentation result = userService.authenticateUser(command);
-
-        return Response
-            .status(Response.Status.OK)
-            .entity(result)
-            .build();
-    }
-
-    @GET
-    @Path("/balance")
-    public Response getBalance(@Context SecurityContext ctx) {
-        JsonWebToken jwt = (JsonWebToken) ctx.getUserPrincipal();
-        if (jwt == null) {
-            throw new InvalidCredentialsException("Missing Authorization header with JWT token");
-        }
-        Long authenticatedUserId = Long.valueOf(jwt.getClaim("user_id").toString());
-
-        BalanceRepresentation result = userService.getBalance(authenticatedUserId);
 
         return Response
             .status(Response.Status.OK)
